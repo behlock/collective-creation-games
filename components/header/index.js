28a284dafe59977c3a000fdfcd6330664ae098ee
behlock <behlocks@gmail.com>
2023-01-28 21:11:44 +0200

    Force graph setup

--- components/header/index.js
+++ components/header/index.js
@@ -4,5 +4,23 @@ export const Header = () => {
   // const isMobile = useMediaQuery('(max-width: 800px)')
   // const visible = usePageAppear()
 
-  return <header className={s.container}></header>
+  return (
+    <header className={s.container}>
+      <div className={s.title}>
+        <a href="/">
+          <h3>Collective Creation Games</h3>
+        </a>
+      </div>
+      {/* <div className={s.nav}>
+        <ul>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div> */}
+    </header>
+  )
 }
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,13 @@
+import dynamic from 'next/dynamic'
+
 import { ClientOnly } from 'components/isomorphic'
 import { content } from 'content/content'
+import miserables from 'content/miserables.json'
 import { Layout } from 'layouts/default'
+
+const ForceGraph2D = dynamic(() => import('react-force-graph-2d'), {
+  ssr: false,
+})
 
 export default function Home({ footer }) {
   // const isDesktop = useMediaQuery('(min-width: 800px)')
@@ -8,6 +15,30 @@ export default function Home({ footer }) {
   return (
     <Layout footerLinks={footer.links}>
       {/* {!isDesktop ? <LayoutMobile  /> : <ClientOnly />} */}
+      <ForceGraph2D
+        graphData={miserables}
+        nodeId="id"
+        nodeVal="group"
+        nodeTitle="id"
+        linkWidth="value"
+        width={1152}
+        height={600}
+        nodeColor={(node) => {}}
+        linkDirectionalParticles={2}
+        linkDirectionalParticleWidth={1}
+        linkDirectionalParticleSpeed={0.01}
+        linkDirectionalParticleColor={(link) => {
+          if (link.value > 0.5) {
+            return 'red'
+          } else {
+            return 'blue'
+          }
+        }}
+        nodeLabel={(node) => {
+          return node.id
+        }}
+      />
+
       <ClientOnly />
     </Layout>
   )
