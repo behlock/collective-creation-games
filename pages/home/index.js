7b707af81144c3e624beff602344b2d14a9b5382
behlock <behlocks@gmail.com>
2023-01-29 07:38:25 +0200

    Use 3d graph

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,51 +1,61 @@
 import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import SpriteText from 'three-spritetext'
 
 import { ClientOnly } from 'components/isomorphic'
 import { content } from 'content/content'
 import miserables from 'content/miserables.json'
 import { Layout } from 'layouts/default'
 
-const ForceGraph2D = dynamic(() => import('react-force-graph-2d'), {
+const ForceGraph3D = dynamic(() => import('react-force-graph-3d'), {
   ssr: false,
 })
 
 export default function Home({ footer }) {
   // const isDesktop = useMediaQuery('(min-width: 800px)')
 
+  // store state of hidden nodes
+
+  const [hiddenNodes, setHiddenNodes] = useState([])
+
   return (
     <Layout footerLinks={footer.links}>
       {/* {!isDesktop ? <LayoutMobile  /> : <ClientOnly />} */}
       <ClientOnly />
-      <ForceGraph2D
+      <ForceGraph3D
+        // DATA
         graphData={miserables}
-        // width={1152}
-        // height={600}
-        backgroundColor="white"
-        nodeColor={(node) => {
-          if (node.group === 1) {
-            return 'red'
-          } else {
-            return 'blue'
-          }
-        }}
-        nodeRelSize={4}
-        linkWidth={1}
-        linkColor={(link) => {
-          if (link.value > 0.5) {
-            return 'red'
-          } else {
-            return 'blue'
-          }
-        }}
-        onNodeClick={(node) => {
-          console.log(node)
-        }}
+        // CONTAINER
+        backgroundColor="black"
+        // NODES
+        nodeRelSize={8}
         nodeVal={(node) => {
           node.group
         }}
-        nodeLabel={(node) => {
-          return node.id
+        nodeLabel={() => ''}
+        nodeThreeObject={(node) => {
+          const sprite = new SpriteText(node.id)
+          sprite.color = node.color
+          sprite.textHeight = 8
+          return sprite
         }}
+        nodeAutoColorBy="group"
+        onNodeClick={(node) => {
+          if (hiddenNodes.includes(node.id)) {
+            setHiddenNodes(hiddenNodes.filter((id) => id !== node.id))
+          } else {
+            setHiddenNodes([...hiddenNodes, node.id])
+          }
+        }}
+        nodeVisibility={(node) => {
+          return !hiddenNodes.includes(node.id)
+        }}
+        // LINKS
+        // linkWidth={2}
+        linkColor="black"
+        linkDirectionalArrowLength={3.5}
+        linkDirectionalArrowRelPos={1}
+        linkCurvature={0.25}
       />
     </Layout>
   )
