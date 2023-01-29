fdc9d88c45c60f1a71b0a4afc605e587f057a16a
behlock <behlocks@gmail.com>
2023-01-29 08:11:19 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/*.{html,js}', './components/*.{html,js}', './layouts/*.{html,js}', './content/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
