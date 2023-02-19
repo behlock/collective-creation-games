5c305d7ae94db16b795e5b48b80990512ba935e0
behlock <behlocks@gmail.com>
2023-02-19 20:42:53 +0200

    get rid of margin on canvas

--- components/force-graph/force-graph.js
+++ components/force-graph/force-graph.js
@@ -154,61 +154,59 @@ export const ForceGraph = ({ graphData }) => {
         //   setVisibleNodes(graphData.nodes.map((node) => node.id))
         // }}
       />
-      <div className="mx-20">
-        <ForceGraph3D
-          // RENDERING
-          extraRenderers={extraRenderers}
-          // DATA
-          graphData={graphData}
-          nodeId="id"
-          linkSource="source"
-          linkTarget="target"
-          // CONTAINER
-          backgroundColor="black"
-          showNavInfo={false}
-          // NODES
-          nodeRelSize={10}
-          nodeVal={(node) => {
-            node.group
-          }}
-          nodeLabel={() => ''}
-          nodeThreeObject={(node) => {
-            const nodeEl = document.createElement('div')
-            nodeEl.textContent = node.id
-            nodeEl.style.color = isTooLightYIQ(node.color) ? '#000' : '#fff'
-            nodeEl.style.fontSize = '0.75rem'
-            nodeEl.style.opacity = '0.5'
-            nodeEl.style.userSelect = 'none'
-            nodeEl.style.textAlign = 'center'
-            nodeEl.style.width = '100px'
-            // nodeEl.className = s.nodeLabel
-            return new CSS2DObject(nodeEl)
-          }}
-          nodeThreeObjectExtend={true}
-          nodeAutoColorBy={(node) => node.color}
-          nodeVisibility={(node) => visibleNodes.includes(node.id)}
-          nodeOpacity={1}
-          nodeResolution={32}
-          // ACTIONS
-          onNodeClick={handleNodeClick}
-          // onNodeClick={(node) => {
-          //   // Aim at node from outside it
-          //   const distance = 40
-          //   const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z)
+      <ForceGraph3D
+        // RENDERING
+        extraRenderers={extraRenderers}
+        // DATA
+        graphData={graphData}
+        nodeId="id"
+        linkSource="source"
+        linkTarget="target"
+        // CONTAINER
+        backgroundColor="black"
+        showNavInfo={false}
+        // NODES
+        nodeRelSize={10}
+        nodeVal={(node) => {
+          node.group
+        }}
+        nodeLabel={() => ''}
+        nodeThreeObject={(node) => {
+          const nodeEl = document.createElement('div')
+          nodeEl.textContent = node.id
+          nodeEl.style.color = isTooLightYIQ(node.color) ? '#000' : '#fff'
+          nodeEl.style.fontSize = '0.75rem'
+          nodeEl.style.opacity = '0.5'
+          nodeEl.style.userSelect = 'none'
+          nodeEl.style.textAlign = 'center'
+          nodeEl.style.width = '100px'
+          // nodeEl.className = s.nodeLabel
+          return new CSS2DObject(nodeEl)
+        }}
+        nodeThreeObjectExtend={true}
+        nodeAutoColorBy={(node) => node.color}
+        nodeVisibility={(node) => visibleNodes.includes(node.id)}
+        nodeOpacity={1}
+        nodeResolution={32}
+        // ACTIONS
+        onNodeClick={handleNodeClick}
+        // onNodeClick={(node) => {
+        //   // Aim at node from outside it
+        //   const distance = 40
+        //   const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z)
 
-          //   const newPos =
-          //     node.x || node.y || node.z
-          //       ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
-          //       : { x: 0, y: 0, z: distance } // special case if node is in (0,0,0)
+        //   const newPos =
+        //     node.x || node.y || node.z
+        //       ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+        //       : { x: 0, y: 0, z: distance } // special case if node is in (0,0,0)
 
-          //   this.cameraPosition(
-          //     newPos, // new position
-          //     node, // lookAt ({ x, y, z })
-          //     3000 // ms transition duration
-          //   )
-          // }}
-        />
-      </div>
+        //   this.cameraPosition(
+        //     newPos, // new position
+        //     node, // lookAt ({ x, y, z })
+        //     3000 // ms transition duration
+        //   )
+        // }}
+      />
     </>
   )
 }
