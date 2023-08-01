4aaa1a7bd7cfe17d60db3283add5154781da7c69
behlock <behlocks@gmail.com>
2023-08-01 11:54:13 +0100

    move stats tracking url to env var

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import Script from 'next/script'
 import { CookiesProvider } from 'react-cookie'
 
 import '@/styles/global.scss'
+import { config } from '@/utils/config'
 
 // @ts-ignore
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script src="https://stats-peach-alpha.vercel.app/api/stats.js" />
+      <Script src={config.STATS_TRACKING_URL} />
       <CookiesProvider>
         <Component {...pageProps} />
       </CookiesProvider>
---
+++ src/utils/config.ts
@@ -0,0 +1,3 @@
+export const config: { [key: string]: string } = {
+  STATS_TRACKING_URL: process.env.NEXT_PUBLIC_STATS_TRACKING_URL || '',
+}
